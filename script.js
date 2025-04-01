9e7f1e52d714915cd702d68d6421b95539ec0214
Mikael <168585927+mikezequinox@users.noreply.github.com>
2025-04-01 14:39:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("mapa").addEventListener("click", (e) => {
+    const tesouro = document.getElementById("tesouro");
+    const distancia = Math.sqrt(
+    Math.pow(e.clientX- tesouro.offsetLeft, 2) + Math.pow(e.clientY- tesouro.offsetTop, 2));
+    
+    if (distancia < 20) {
+            alert("Voc achou o tesouro!");
+        } else if (distancia < 50) {
+            alert("Est muito quente!");
+        } else {
+            alert("Est frio... continue procurando!");
+        }
+});
